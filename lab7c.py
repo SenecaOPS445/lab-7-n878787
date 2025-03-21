38522c34f2d089e19c1d14ecfb123997706aee15
Nawaf RIhani <nrihani@myseneca.ca>
2025-03-20 20:07:55 -0400

    Finished Lab 7

--- lab7c.py
+++ lab7c.py
@@ -16,20 +16,30 @@ def format_time(t):
 
 def sum_times(t1, t2):
     """Add two time objests and return the sum."""
-    sum = Time(0,0,0)
-    sum.hour = t1.hour + t2.hour
-    sum.minute = t1.minute + t2.minute
-    sum.second = t1.second + t2.second
-
-    if sum.second >= 60:
-        sum.minute += sum.second // 60
-        sum.second = sum.second % 60
-
-    if sum.minute >= 60:
-        sum.hour += sum.minute // 60
-        sum.minute = sum.minute % 60
+    total_seconds = time_to_sec(t1) + time_to_sec(t2)
+    return sec_to_time(total_seconds)
 
     return sum
+
+def change_time(time, seconds):
+    total_seconds = time_to_sec(time) + seconds
+    new_time = sec_to_time(total_seconds)
+
+    time.hour, time.minute, time.second = new_time.hour, new_time.minute, new_time.second
+    return None
+
+def time_to_sec(time):
+    '''convert a time object to a single integer representing the number of seconds from mid-night'''
+    minutes = time.hour * 60 + time.minute
+    seconds = minutes * 60 + time.second
+    return seconds
+
+def sec_to_time(seconds):   
+    '''convert a given number of seconds to a time object in hour,minute,second format'''
+    time = Time()
+    minutes, time.second = divmod(seconds, 60)
+    time.hour, time.minute = divmod(minutes,60)
+    return time
 
 def valid_time(t):
     """check for the validity of the time object attributes:
